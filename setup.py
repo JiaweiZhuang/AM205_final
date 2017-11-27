67659fc0ba5ac5d2f5c481541e9d7cca291109dc
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-11-27 11:21:23 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='neuralsolver',
+      version='0.0.1',
+      description='Solve ODE by neural network',
+      url='https://github.com/JiaweiZhuang/AM205_final',
+      packages=['neuralsolver'],
+      install_requires=['autograd==1.2', 'scipy==1.0']
+      )
